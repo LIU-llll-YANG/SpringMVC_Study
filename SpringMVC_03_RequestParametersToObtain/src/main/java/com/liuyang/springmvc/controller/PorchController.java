a4243b313202f63e4c0f4b2fe4c92f43481a1f1f
LIUYANG <yang.china.0201@gmail.com>
2023-02-28 13:59:04 +0800

    SpringMVC请求参数

---
+++ SpringMVC_03_RequestParametersToObtain/src/main/java/com/liuyang/springmvc/controller/PorchController.java
@@ -0,0 +1,12 @@
+package com.liuyang.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PorchController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
